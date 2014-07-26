6020e4c23efba2bf7f49efa9b231ba13c712f28b
Alex Jordan <alexander3223098@gmail.com>
2014-07-26 01:05:08 -0700

    simplify the Apache configs

--- apache-config/apache-config.pp
+++ apache-config/apache-config.pp
@@ -17,20 +17,13 @@ include apache::mod::deflate
 include apache::mod::ssl
 
 apache::vhost { 'strugee.net':
-  port     => 443,
-  docroot  => '/srv/http/default/',
-  ssl	   => true,
-  ssl_cert => '/etc/ssl/certs/mailserver.pem',
-  ssl_key  => '/etc/ssl/private/mailserver.pem',
-}
-
-apache::vhost { 'www.strugee.net':
-  port     => 80,
-  docroot  => '/srv/http/default/',
-}
-
-apache::vhost { 'ssh.strugee.net':
-  port     => 80,
-  docroot  => '/srv/http/default/',
-  default_vhost => true,
+  port          => 443,
+  docroot       => '/srv/http/default/',
+  serveraliases => [
+    'www.strugee.net',
+    'ssh.strugee.net',
+  ],
+  ssl	        => true,
+  ssl_cert      => '/etc/ssl/certs/mailserver.pem',
+  ssl_key       => '/etc/ssl/private/mailserver.pem',
 }
